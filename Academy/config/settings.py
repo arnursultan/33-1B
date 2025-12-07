b70ba7910311e22778316ddcb9d816a700532999
arnursultan <badasspubgm@gmail.com>
2025-12-07 13:05:32 +0600

    update lesson 7 code

--- Academy/config/settings.py
+++ Academy/config/settings.py
@@ -1,7 +1,7 @@
 DB = {
     "host": "localhost",
     "port": 5433,
-    "database": "academy",
-    "user": "postgres",
-    "password": "Nursultan04",
+    "database": "...",
+    "user": "...",
+    "password": "...",
 }
